77c7e703748fa884ddc63d72df25a248d1171042
maciek <macieq2048@gmail.com>
2022-01-04 10:28:45 +0100

    asd

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
-
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
 
 
 def main():
-    pass
+    bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
+
+    sct = mss()
+
+    while True:
+        sct_img = sct.grab(bounding_box)
+        cv2.imshow('screen', np.array(sct_img))
+
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
 if __name__ == '__main__':
     main()
+
+
