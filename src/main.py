2d95ac627f2f449848c14e456478ff6295ad63df
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2022-01-04 11:05:28 +0100

    add rect detection

--- src/main.py
+++ src/main.py
@@ -10,8 +10,10 @@ def main():
     sct = mss()
 
     while True:
-        frame = sct.grab(bounding_box)
-        cv2.imshow('screen', np.array(frame))
+        frame_raw = sct.grab(bounding_box)
+        frame = np.array(frame_raw)
+        detect_rectangles(frame)
+        
         
         
 
@@ -20,6 +22,28 @@ def main():
         if (cv2.waitKey(1) & 0xFF) == ord('q'):
             cv2.destroyAllWindows()
             break
+        
+
+def detect_rectangles(img):
+    # convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    ret, thresh1 = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
+
+    # Find the contours
+    contours, hierarchy = cv2.findContours(thresh1,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+    # For each contour, find the bounding rectangle and draw it
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(img,
+                      (x, y), (x + w, y + h),
+                      (0, 255, 0),
+                      2)
+
+    cv2.imshow('img', img)
 
 if __name__ == '__main__':
     main()
