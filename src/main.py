e32878260c6b554f5a85d08bc3910712e51834e3
maciek <macieq2048@gmail.com>
2022-01-04 10:58:36 +0100

    basic edge detection

--- src/main.py
+++ src/main.py
@@ -5,17 +5,33 @@ from PIL import Image
 
 
 def main():
-    bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
 
-    sct = mss()
+    # Load the image
+    img = cv2.imread('sample.png')
 
-    while True:
-        sct_img = sct.grab(bounding_box)
-        cv2.imshow('screen', np.array(sct_img))
+    # convert to grayscale
 
-        if (cv2.waitKey(1) & 0xFF) == ord('q'):
-            cv2.destroyAllWindows()
-            break
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('egdes', gray)
+    ret, thresh1 = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
+
+
+    # Find the contours
+    contours, hierarchy = cv2.findContours(thresh1,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+    # For each contour, find the bounding rectangle and draw it
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(img,
+                      (x, y), (x + w, y + h),
+                      (0, 255, 0),
+                      2)
+
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
