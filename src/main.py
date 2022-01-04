1dfefc9dd979b0cf0bf100c7456fabe26e41de2d
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2022-01-04 11:43:38 +0100

    add player steering

--- src/main.py
+++ src/main.py
@@ -2,13 +2,49 @@ import numpy as np
 import cv2
 from mss import mss
 from PIL import Image
+from pynput.keyboard import Key, Controller, Listener
+
+def move_player(keyboard, move=None):
+    if move is None:
+        return
+    
+    if move == 'left':
+        keyboard.press(Key.left)
+        keyboard.release(Key.left)
+        
+    elif move == 'right':
+        keyboard.press(Key.right)
+        keyboard.release(Key.right)
+    
+    elif move == 'shoot':
+        keyboard.press(Key.space)
+        keyboard.release(Key.space)
+    
+
+def start_on_enter_press(key):
+    if key == Key.enter:
+        print("ENTER ENTER")
+        return False
+    # # Collect events until released
+    # with Listener(
+    #         on_press=start_on_enter_press,
+    #         on_release=None) as listener:
+    #     listener.join()
+
+
 
 
 def main():
+    
     bounding_box = {'top': 8, 'left': 8, 'width': 960, 'height': 540}
-
+    keyboard = Controller()
+    
     sct = mss()
+    
+    frame_raw = sct.grab(bounding_box)
+    frame = np.array(frame_raw)
 
+    
     while True:
         frame_raw = sct.grab(bounding_box)
         frame = np.array(frame_raw)
@@ -16,6 +52,7 @@ def main():
         
         
         
+        # move_player(keyboard, 'shoot')                                                                          
 
 
 
