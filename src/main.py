470f8b5e3a5a125f29668fa8ecb74d373be012da
maciek <macieq2048@gmail.com>
2022-01-04 12:53:37 +0100

    basic position updates

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from typing import Tuple, Any
+
 import numpy as np
 import cv2
 from mss import mss
@@ -80,18 +82,22 @@ class GameObject:
         self.direction = 1
         self.position = position
 
-    def updatePosition(self, new_pos: Vector2D):
+    def updatePosition(self, new_pos: Vector2D) -> Vector2D:
         self.direction = 1 if new_pos.x > self.position.x else -1
         self.position = new_pos
         return self.position
+
+    def getPosition(self) -> Vector2D:
+        return self.position
+
+    def getPositionTuple(self) -> tuple[Any, Any]:
+        return int(self.position.x), int(self.position.y)
 
     def getHeight(self):
         pass  # TODO return height from the player
 
     def getDirection(self):
         return self.direction
-    
-    
 
 
 class Enemy(GameObject):
@@ -110,13 +116,18 @@ class Player(GameObject):
 
     # TODO move player
 
+
 class Game:
     keyboard = None
     sct = None
+    bounding_box = {'top': 8, 'left': 8, 'width': 960, 'height': 540}
+
     def __init__(self):
         self.keyboard = Controller()
         self.sct = mss()
-        
+        self.grabbedFrame = None
+        self.detectedContours = None
+
         self.bullets = []
         self.enemies = []
         self.player = Player(Vector2D(0, 0))
@@ -137,17 +148,34 @@ class Game:
             self.keyboard.press(Key.space)
             self.keyboard.release(Key.space)
 
+    def showDebugImage(self):
+        debug_img = self.grabbedFrame
+        for cnt in self.detectedContours:
+            (x, y, w, h) = cnt
+            cv2.rectangle(debug_img,
+                          (x, y), (x + w, y + h),
+                          (0, 255, 0),
+                          2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
 
-    def debugImage(img, contours):
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(img,
-                        (x, y), (x + w, y + h),
-                        (0, 255, 0),
-                        2)
 
-    def detect_rectangles(self, img):
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        font_scale = 0.7
+        print(self.player.getPositionTuple())
+        color = (255, 0, 255)
+        cv2.putText(debug_img, 'pl', self.player.getPositionTuple(), font,
+                            font_scale, color, 1, cv2.LINE_AA)
+
+        for bullet in self.bullets:
+            cv2.putText(debug_img, 'bul', bullet.getPositionTuple(), font,
+                        font_scale, color, 1, cv2.LINE_AA)
+
+        for enemy in self.enemies:
+            cv2.putText(debug_img, 'enm', enemy.getPositionTuple(), font,
+                        font_scale, color, 1, cv2.LINE_AA)
+        cv2.imshow("debug image", debug_img)
+        
+    def detect_contours(self):
+        gray = cv2.cvtColor(self.grabbedFrame, cv2.COLOR_BGR2GRAY)
 
         ret, thresh1 = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
 
@@ -155,89 +183,42 @@ class Game:
         contours, hierarchy = cv2.findContours(
             thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # for cnt in contours:
-        #     x, y, w, h = cv2.boundingRect(cnt)
-        #     cv2.rectangle(img,
-        #                   (x, y), (x + w, y + h),
-        #                   (0, 255, 0),
-        #                   2)
+        self.detectedContours = [cv2.boundingRect(contour) for contour in contours]
 
-        return [cv2.boundingRect(contour) for contour in contours], img
-    
-    def labelObjects(self, contours):
-        self.enemiesPositions = [ob for ob in contours if 50 <
-                        ob[2] < 70]  # get big objects in x dir
-        bulletsPositions = [ob for ob in contours if 4 <
-                        ob[2] < 8]  # get smol objects in x dir
+    def updatePositions(self):
+        print(self.detectedContours)
+        enemies_positions = [ob for ob in self.detectedContours if 50 < ob[2] < 70]  # get big objects in x dir
+        bullets_positions = [ob for ob in self.detectedContours if 6 < ob[2] < 15]  # get smol objects in x dir
 
-        playerPosition = max(contours, key=lambda x: x[1])
+        player_idx, player_contour = max(enumerate(enemies_positions), key=lambda x: x[1][1])
+        enemies_positions.pop(player_idx)
+        self.enemies = [Enemy(Vector2D(cont[0] + cont[2] / 2, cont[1] + cont[3] / 2)) for cont in enemies_positions]
+        self.bullets = [Bullet(Vector2D(cont[0] + cont[2] / 2, cont[1] + cont[3] / 2)) for cont in bullets_positions]
 
-        return playerPosition, enemiesPositions, bulletsPositions
-
-    def updateEnemiesPositions(self, contours):
-        return [Enemy(Vector2D(cont[0] + cont[2]/2, cont[1] + cont[3]/2)) for cont in contours]
-
-
-    def updateBulletsPositions(self, contours):
-        return [Bullet(Vector2D(cont[0] + cont[2]/2, cont[1] + cont[3]/2)) for cont in contours]
-
-
-    def updatePlayerPosition(self, contour):
-        if len(contour) > 1:
+        if len(player_contour) > 1:
             print("DETECTED MORE THEN 1 PLAYER")
-        return Player(Vector2D(contour[0], contour[1]))
+        self.player = Player(Vector2D(player_contour[0], player_contour[1]))
 
-
-    def updatePositions(self, contours):
-        player_position, enemies_positions, bullets_positions = labelObjects(
-            contours)
-        print(player_position, bullets_positions, enemies_positions)
-
-        enemies = updateEnemiesPositions(enemies_positions)
-        bullets = updateBulletsPositions(bullets_positions)
-        player = updatePlayerPosition(player_position)
-        return player, enemies, bullets
+    def getFrame(self):
+        frame_raw = self.sct.grab(self.bounding_box)
+        # self.grabbedFrame = np.array(frame_raw)
+        self.grabbedFrame = cv2.imread("sample.png")
 
     def main(self):
-
-        bounding_box = {'top': 8, 'left': 8, 'width': 960, 'height': 540}
-
-        frame_raw = sct.grab(bounding_box)
-        frame = np.array(frame_raw)
+        self.getFrame()
 
         while True:
-            frame_raw = sct.grab(bounding_box)
-            frame = np.array(frame_raw)
-            detect_rectangles(frame)
-
+            self.getFrame()
+            self.detect_contours()
             # move_player(keyboard, 'shoot')
-            frame = cv2.imread("sample.png")
-            contours, img = detect_rectangles(frame)
-            cv2.imshow('debug image', img)
-            updatePositions(contours)
+            self.updatePositions()
+            self.showDebugImage()
 
             if (cv2.waitKey(1) & 0xFF) == ord('q'):
                 cv2.destroyAllWindows()
                 break
 
 
-
-
-
-
-# [(283, 537, 8, 3), (741, 536, 8, 4), (362, 520, 60, 20), (768, 479, 8, 21), (665, 429, 8, 20),
-# (622, 405, 8, 21), (389, 337, 8, 20), (213, 334, 8, 21), (807, 252, 60, 19), (153, 241, 60, 21),
-# (395, 226, 60, 21), (532, 225, 60, 21), (537, 134, 60, 20), (618, 119, 60, 21), (46, 37, 3, 3), (46, 31, 3, 3),
-# (54, 28, 9, 13), (56, 30, 5, 9), (17, 28, 3, 2), (9, 28, 36, 13), (39, 36, 4, 3), (31, 36, 4, 3), (36, 33, 5, 5),
-# (39, 32, 4, 3), (31, 32, 4, 3), (51, 17, 3, 3), (51, 11, 3, 3), (19, 11, 31, 10), (43, 16, 5, 3), (44, 12, 3, 3),
-# (28, 12, 6, 7), (20, 12, 8, 7), (59, 8, 9, 13), (60, 10, 6, 9), (8, 7, 10, 14), (960, 0, 1, 540)]
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.main()
